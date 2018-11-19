66698bfc23a166a941a22c6a26112904da89ae03
Pedro Miranda <gamesdarksoul@gmail.com>
2018-11-18 22:52:56 -0300

    Visual Definitivo v0.0.5

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function login(){
     var RA = document.getElementById("inputRA").value;
     var senha = document.getElementById("inputPassword").value;
 
-    if(RA != "" && senha != ""){
+    if(RA != null && senha != "null"){
         return true;
     }else{
         window.alert("Algo deu errado, redigite seu RA e/ou senha");
